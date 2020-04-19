677fc908e738e0fbdf174fedd34e011d6f6cad46
gururajb66 <gururajb66@gmail.com>
2020-04-19 13:01:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const server = http.createServer((request, resp) => {
+ if (req.url === "/create") {
+        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
 });
 
 const port = process.env.PORT || 1337;
